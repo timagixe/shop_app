6fa087ee78913288c9be46ea3abb98cceacb2fb1
Tima Gixe <timagixe@protonmail.com>
2021-03-18 09:28:46 +0200

    feat(models): :sparkles: create product model

---
+++ lib/models/product.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/foundation.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final String imageUrl;
+  final double price;
+  bool isFavorite = false;
+
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.imageUrl,
+    @required this.price,
+  })  : assert(id != null),
+        assert(title != null),
+        assert(description != null),
+        assert(imageUrl != null),
+        assert(price != null);
+}
