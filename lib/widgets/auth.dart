0335241c46f75cd7016006cda3422ae8409a45c7
Tima Gixe <timagixe@protonmail.com>
2021-04-05 22:39:26 +0300

    feat(widgets): :dizzy: improve animations on auth screen

--- lib/widgets/auth.dart
+++ lib/widgets/auth.dart
@@ -26,7 +26,7 @@ class _AuthWidgetState extends State<AuthWidget>
   var _isLoading = false;
   final _passwordController = TextEditingController();
   AnimationController _animationController;
-  Animation<Size> _heightAnimation;
+  Animation<double> _opacityAnimation;
 
   @override
   void initState() {
@@ -38,10 +38,7 @@ class _AuthWidgetState extends State<AuthWidget>
       ),
     );
 
-    _heightAnimation = Tween(
-      begin: Size(double.infinity, kMinAuthWidgetHeight),
-      end: Size(double.infinity, kMaxAuthWidgetHeight),
-    ).animate(
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
       CurvedAnimation(
         parent: _animationController,
         curve: Curves.linear,
@@ -119,15 +116,18 @@ class _AuthWidgetState extends State<AuthWidget>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (context, child) => Container(
-          height: _heightAnimation.value.height,
-          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-          width: deviceSize.width * 0.75,
-          padding: EdgeInsets.all(16.0),
-          child: child,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.linear,
+        height: _authMode == AuthMode.Login
+            ? kMinAuthWidgetHeight
+            : kMaxAuthWidgetHeight,
+        constraints: BoxConstraints(
+          minHeight: kMinAuthWidgetHeight,
+          maxHeight: kMaxAuthWidgetHeight,
         ),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -160,20 +160,31 @@ class _AuthWidgetState extends State<AuthWidget>
                     _authData['password'] = value;
                   },
                 ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
-                    decoration: InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.Signup
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return 'Passwords do not match!';
-                            }
-                            return null;
-                          }
-                        : null,
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.linear,
+                  constraints: BoxConstraints(
+                    minHeight: _authMode == AuthMode.Login ? 0 : 60,
+                    maxHeight: _authMode == AuthMode.Login ? 0 : 120,
                   ),
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: TextFormField(
+                      enabled: _authMode == AuthMode.Signup,
+                      decoration:
+                          InputDecoration(labelText: 'Confirm Password'),
+                      obscureText: true,
+                      validator: _authMode == AuthMode.Signup
+                          ? (value) {
+                              if (value != _passwordController.text) {
+                                return 'Passwords do not match!';
+                              }
+                              return null;
+                            }
+                          : null,
+                    ),
+                  ),
+                ),
                 SizedBox(
                   height: 20,
                 ),
