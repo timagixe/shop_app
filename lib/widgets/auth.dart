21cf626a2e87c868d5f7777b9ec1adbcec2a27c4
Tima Gixe <timagixe@protonmail.com>
2021-04-05 22:22:15 +0300

    feat(widgets): :sparkles: add animation on signup / signin widget swap

--- lib/widgets/auth.dart
+++ lib/widgets/auth.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/models/http_exceotion.dart';
 
 import '../providers/auth.dart';
 
 enum AuthMode { Signup, Login }
 
+const double kMinAuthWidgetHeight = 260.0;
+const double kMaxAuthWidgetHeight = 320.0;
+
 class AuthWidget extends StatefulWidget {
-  const AuthWidget({
-    Key key,
-  }) : super(key: key);
+  const AuthWidget();
 
   @override
   _AuthWidgetState createState() => _AuthWidgetState();
 }
 
-class _AuthWidgetState extends State<AuthWidget> {
+class _AuthWidgetState extends State<AuthWidget>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
@@ -24,6 +25,29 @@ class _AuthWidgetState extends State<AuthWidget> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  AnimationController _animationController;
+  Animation<Size> _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 300,
+      ),
+    );
+
+    _heightAnimation = Tween(
+      begin: Size(double.infinity, kMinAuthWidgetHeight),
+      end: Size(double.infinity, kMaxAuthWidgetHeight),
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.linear,
+      ),
+    );
+  }
 
   void _submit() async {
     if (!_formKey.currentState.validate()) {
@@ -60,10 +84,12 @@ class _AuthWidgetState extends State<AuthWidget> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      _animationController.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _animationController.reverse();
     }
   }
 
@@ -93,12 +119,15 @@ class _AuthWidgetState extends State<AuthWidget> {
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (context, child) => Container(
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: child,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -177,5 +206,11 @@ class _AuthWidgetState extends State<AuthWidget> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
   }
 }
